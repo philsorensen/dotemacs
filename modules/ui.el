ff152d4ba44c22f3f0aa61c037175afb7d9e8aba
Phillip Sorensen <phil.a.sorensen@gmail.com>
2022-01-15 23:12:30 -0500

    Add all-the-icons and initial doom-modeline setup

--- modules/ui.el
+++ modules/ui.el
@@ -17,10 +17,26 @@
 (unless (display-graphic-p)
   (menu-bar-mode 0))
 
+;; install 'all-the-icons' fonts
+(setup (:package all-the-icons)
+  (let ((font-file (concat (or (getenv "XDG_DATA_HOME")
+			       (concat (getenv "HOME") "/.local/share/"))
+			   "fonts/all-the-icons.ttf")))
+    (unless (file-exists-p font-file)
+      (all-the-icons-install-fonts t))))
 
-
+;; set theme
 (setup (:package doom-themes)
   (load-theme 'doom-dark+ t))
+
+;; doom-modeline
+(setup (:package doom-modeline)
+  (:option doom-modeline-height 15
+           doom-modeline-bar-width 6
+	   doom-modeline-minor-modes t
+	   doom-modeline-buffer-file-name-style 'file-name)
+  (:with-hook after-init-hook
+    (:hook doom-modeline-init)))
 
 
 (provide 'ui)
