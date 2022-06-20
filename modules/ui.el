7b28b101e5898df800ee664d0d339359741bad8a
Phillip Sorensen <phil.a.sorensen@gmail.com>
2022-06-19 20:32:15 -0400

    Customize fill-column-indicator face

--- modules/ui.el
+++ modules/ui.el
@@ -16,14 +16,20 @@
 (unless (display-graphic-p)
   (menu-bar-mode 0))
 
-;; set theme
+;; set theme (customize fill-column-indicator)
 (setup (:package modus-themes)
   (:option modus-themes-bold-constructs t
            modus-themes-italic-constructs t
            modus-themes-syntax '(yellow-comments))
   (modus-themes-load-themes)
   (:with-hook after-init-hook
-    (:hook modus-themes-load-vivendi)))
+    (:hook modus-themes-load-vivendi))
+  (:with-hook modus-themes-after-load-theme-hook
+    (:hook (lambda ()
+             (set-face-attribute
+               'fill-column-indicator nil
+               :background (modus-themes-color 'bg-active))))))
+
 
 (provide 'ui)
 ;;; ui.el ends here
